cf9105d048ea02e047eccbafef3cc9eb513439e9
Henrique Furtado <carloshenrique.furtado@hotmail.com>
2024-03-19 00:47:35 -0300

    Add header and font to project

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import logo from '../assets/images/sf-logo.png';
+import './Header.css'
 
 function Header() {
     return (
         <header>
-            <img src="" alt="" />
+            <img className='logo' src={logo} alt="" />
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Clientes</a></li>
+                <li><a href="#">Produtos</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Depoimentos</a></li>
+                <li><a href="#">Depoimentos</a></li>
+            </ul>
         </header>
     )
 }
