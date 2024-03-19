8830d322c42494ed5f751c90bf4eba11ea18f507
Henrique Furtado <carloshenrique.furtado@hotmail.com>
2024-03-19 00:35:40 -0300

    Starting Project Development

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import './App.css'
+
+function App() {
+
+  return (
+    <main>
+      <Header></Header>
+    </main>
+  )
+}
+
+export default App
